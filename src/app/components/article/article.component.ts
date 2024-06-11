bbbeb7a12ff5c8107a6ec84ae2970e97f2e7104e
Slavik89 <terakrokos@gmail.com>
2024-06-11 18:30:56 +0300

    All article properties has been passed to article component through routing

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-article',
@@ -9,14 +8,16 @@ import { switchMap } from 'rxjs/operators';
 })
 export class ArticleComponent implements OnInit {
 
-  id: any;
+  id!: string | null;
+  article: any;
 
   constructor (private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
-    console.log(this.id);
-    // this.route.params.subscribe(params => console.log(params));
+    
+    this.article = history.state.data;
+    // console.log(this.article);
   }
 
 }
